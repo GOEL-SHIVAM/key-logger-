0d6cf5971e2cba48adc1f0f3204a68d46dda459d
Shivam Goel <131146872+GOEL-SHIVAM@users.noreply.github.com>
2025-01-12 17:50:36 +0000

    done with project

---
+++ script.js
@@ -0,0 +1,6 @@
+// r;
+// const keyOutput = document.getElementById("key-output");
+
+// document.addEventListener("keydown", (event) => {
+//   keyOutput.textContent = `You pressed: ${event.key}`;
+// });
